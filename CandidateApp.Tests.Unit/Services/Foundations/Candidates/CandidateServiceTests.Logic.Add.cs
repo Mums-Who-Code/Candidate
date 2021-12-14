12ce435b270231885aa557a492079c9694985d81
Janane <imjanane@gmail.com>
2021-12-14 15:07:15 -0600

    ShouldAddCandidate -> FAIL

--- CandidateApp.Tests.Unit/Services/Foundations/Candidates/CandidateServiceTests.Logic.Add.cs
+++ CandidateApp.Tests.Unit/Services/Foundations/Candidates/CandidateServiceTests.Logic.Add.cs
@@ -3,6 +3,8 @@
 // ------------------------------------------------
 
 using CandidateApp.ConsoleApp.Models.Candidate;
+using FluentAssertions;
+using Moq;
 using Xunit;
 
 namespace CandidateApp.Tests.Unit.Services.Foundations.Candidates
@@ -15,11 +17,26 @@ namespace CandidateApp.Tests.Unit.Services.Foundations.Candidates
             // given
             Candidate randomCandidate = CreateRandomCandidate();
             Candidate inputCandidate = randomCandidate;
+            Candidate persistedCandidate = inputCandidate;
+            Candidate expectedCandidate = inputCandidate;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertCandidate(inputCandidate))
+                .Returns(persistedCandidate);
 
             // when
-            this.candidateService.AddCandidate(inputCandidate);
+            Candidate actualCandidate = this.candidateService.AddCandidate(inputCandidate);
 
             // then
+            actualCandidate.Should().BeEquivalentTo(expectedCandidate);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.InsertCandidate(inputCandidate),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+
+
         }
     }
 }
